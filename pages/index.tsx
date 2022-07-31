9fe3112bf830becbfbdfe56b70814246b587c2a3
GFX23 <Spacil-Tomas@seznam.cz>
2022-07-31 13:28:21 +0200

    order table modified

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,7 @@
 import { useState } from "react";
 import SortingButton from "../components/toggleButton";
 import { getOrders } from "../utils/firebase/firebase-db";
-
-
-export type Order = {
-  customer: string;
-  date: string;
-  id: string;
-  op1: string | number;
-  op2: string | number;
-  orderNum: string | number;
-  partCount: number;
-  partName: string;
-  price: number;
-  time: string | number;
-};
+import type { Order } from "../utils/types/types";
 
 const Orders: React.FC<{ data: Order[] }> = ({ data }) => {
   const [displayList, setDisplayList] = useState<Order[]>(data);
@@ -22,7 +9,7 @@ const Orders: React.FC<{ data: Order[] }> = ({ data }) => {
   const handleSort = (
     withWhat: keyof Order,
     ascending: boolean | undefined
-  ) => {
+  ): void => {
     const sorted = displayList.slice().sort((a, b) => {
       if (ascending) {
         if (a[withWhat] < b[withWhat]) {
@@ -42,29 +29,72 @@ const Orders: React.FC<{ data: Order[] }> = ({ data }) => {
         }
       }
     });
-    console.log(sorted);
-    console.log(ascending);
-    return sorted;
+    setDisplayList(sorted);
   };
+
   return (
     <>
-    { displayList.map((item, idx) => {
-                  return (
-                    <tr className="hover" key={ idx }>
-                      <th>{ idx }</th>
-                      <td>{ item.date }</td>
-                      <td>{ item.customer }</td>
-                      <td>{ item.orderNum }</td>
-                      <td>{ item.partName }</td>
-                      <td>{ item.partCount }</td>
-                      <td>{ item.price }</td>
-
-                    </tr>
-                  )
-                }) }
-      <SortingButton onClick={handleSort} withWhat="name" />
+    <div className="overflow-x-auto relative shadow-md sm:rounded-lg max-w-5xl mx-auto mt-4">
+      <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+          <tr>
+            <th></th>
+            <th>
+              <label className="flex gap-2">
+                Date <SortingButton onClick={handleSort} withWhat="date" />
+              </label>
+            </th>
+            <th>
+              <label className="flex gap-2">
+                Customer{" "}
+                <SortingButton onClick={handleSort} withWhat="customer" />
+              </label>
+            </th>
+            <th>
+              <label className="flex gap-2">
+                Order <SortingButton onClick={handleSort} withWhat="orderNum" />
+              </label>
+            </th>
+            <th>
+              <label className="flex gap-2">
+                Part name{" "}
+                <SortingButton onClick={handleSort} withWhat="partName" />
+              </label>
+            </th>
+            <th className="text-center">
+              <label className="flex gap-2">
+                Count{" "}
+                <SortingButton onClick={handleSort} withWhat="partCount" />
+              </label>
+            </th>
+            <th>
+              <label className="flex gap-2">
+                Price <SortingButton onClick={handleSort} withWhat="price" />
+              </label>
+            </th>
+            <th className="text-center">DELETE</th>
+            <th className="text-center">UPDATE</th>
+          </tr>
+        </thead>
+        <tbody>
+          {displayList.map((item, idx) => {
+            return (
+              <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600" key={idx}>
+                <th>{idx}</th>
+                <td>{item.date}</td>
+                <td>{item.customer}</td>
+                <td>{item.orderNum}</td>
+                <td>{item.partName}</td>
+                <td>{item.partCount}</td>
+                <td>{item.price}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      </div>
     </>
-    );
+  );
   /*      const [displayList, setDisplayList] = useState([...data.orders])
       const [update, setUpdate] = useState(false)
       const [orderData, setOrderData] = useState({})
@@ -126,39 +156,7 @@ const Orders: React.FC<{ data: Order[] }> = ({ data }) => {
             </div>
           </div>
           { update && <UpdateOrder setUpdate={ setUpdate } orderData={ orderData } id={ itemId } /> }
-          <div className="overflow-x-auto">
-            <table className="table table-zebra table-compact w-full">
-              <thead>
-                <tr>
-                  <th></th>
-                  <th><label className="flex gap-2">Date <SortingButton onClick={ handleSort } withWhat="date" /></label></th>
-                  <th><label className="flex gap-2">Customer <SortingButton onClick={ handleSort } withWhat="customer" /></label></th>
-                  <th><label className="flex gap-2">Order <SortingButton onClick={ handleSort } withWhat="orderNum" /></label></th>
-                  <th><label className="flex gap-2">Part name <SortingButton onClick={ handleSort } withWhat="partName" /></label></th>
-                  <th className="text-center"><label className="flex gap-2">Count <SortingButton onClick={ handleSort } withWhat="partCount" /></label></th>
-                  <th><label className="flex gap-2">Price <SortingButton onClick={ handleSort } withWhat="price" /></label></th>
-                  <th className="text-center">DELETE</th>
-                  <th className="text-center">UPDATE</th>
-                </tr>
-              </thead>
-              <tbody>
-                { displayList.map((item, idx) => {
-                  return (
-                    <tr className="hover" key={ idx }>
-                      <th>{ idx }</th>
-                      <td>{ item.date }</td>
-                      <td>{ item.customer }</td>
-                      <td>{ item.orderNum }</td>
-                      <td>{ item.partName }</td>
-                      <td>{ item.partCount }</td>
-                      <td>{ item.price }</td>
-                      <td className="text-center flex justify-center"><DeleteButton onClick={ () => handleDelete(item.id) } /></td>
-                      <td className="text-center"><UpdateButton onClick={ () => handleUpdate(item.id) } /></td>
-                    </tr>
-                  )
-                }) }
-              </tbody>
-            </table>
+          
             <div className="flex flex-row justify-around">
               <p>TOTAL PART COUNT: {getTotal(displayList, "partCount")}</p>
               <p>TOTAL PRICE: {getTotal(displayList, "price")}</p>
---
+++ utils/types/types.ts
@@ -0,0 +1,13 @@
+export type Order = {
+  customer: string;
+  date: string;
+  id: string;
+  op1: string | number;
+  op2: string | number;
+  orderNum: string | number;
+  partCount: number;
+  partName: string;
+  price: number;
+  time: string | number;
+};
+
