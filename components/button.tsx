d0a8db58a8d95005b826d3e78c1bc0197e71157b
GFX23 <Spacil-Tomas@seznam.cz>
2022-08-06 20:08:56 +0200

    added ref option

--- components/button.tsx
+++ components/button.tsx
@@ -1,15 +1,13 @@
 
 interface ButtonProps{
   name: string,
-  onClick: (e: React.MouseEvent<HTMLButtonElement>) => void
+  onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void
 }
 
-const Button = ({name, onClick}: ButtonProps): JSX.Element => {
+const Button: React.FC<ButtonProps> = ({name, onClick}) => {
   return (
-      <button onClick={onClick} className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800">
-        <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+      <button onClick={onClick} className="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
             {name}
-        </span>
       </button>
   )
 }
---
+++ components/select.tsx
@@ -0,0 +1,22 @@
+import { Ref } from "react"
+
+interface SelectProps {
+  options: string[]
+  name: string
+  value: string
+  onChange: (event: React.ChangeEvent<HTMLSelectElement>) => void
+  ref?: Ref<HTMLSelectElement>
+}
+
+const Select: React.FC<SelectProps> = ({options, name, value, onChange, ref}) => {
+  return (
+    <select ref={ref} value={value} onChange={onChange} className="block mb-4 py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200">
+      <option className="text-gray-900">{name}</option>
+      {options.map((option, idx) => {
+        return <option key={idx} value={option}>{option}</option>
+      })}
+    </select>
+  )
+}
+
+export default Select
