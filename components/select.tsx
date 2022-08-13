224928155969ec4c181f9ef5a1766589c04fb99a
GFX23 <Spacil-Tomas@seznam.cz>
2022-08-13 19:10:15 +0200

    added functions

--- components/select.tsx
+++ components/select.tsx
@@ -11,7 +11,7 @@ interface SelectProps {
 const Select: React.JSXElementConstructor<SelectProps> = ({options, name, value, onChange, referer}) => {
   return (
     <select ref={referer} value={value} onChange={onChange} className="block mb-4 py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200">
-      <option className="text-gray-900">{name}</option>
+      <option value="" className="text-gray-900">{name}</option>
       {options.map((option, idx) => {
         return <option key={idx} value={option}>{option}</option>
       })}
