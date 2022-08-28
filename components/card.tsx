61790a21f6eb86e090f73be717eb998bb5f803c7
GFX23 <Spacil-Tomas@seznam.cz>
2022-08-28 09:34:28 +0200

    Update card.tsx

--- components/card.tsx
+++ components/card.tsx
@@ -6,8 +6,8 @@ interface CardProps {
 const Card: React.FC<CardProps> = ({placeholder, text}) => {
   return (
     <div className="block p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-      <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{placeholder}</h5>
-      <div className="font-normal text-gray-700 dark:text-gray-400">{text}</div>
+      <h5 className="font-normal text-gray-700 dark:text-gray-400">{placeholder}</h5>
+      <div className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{text}</div>
     </div>
   );
 };
