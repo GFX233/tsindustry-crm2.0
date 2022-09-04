927365a623acac88b966363fd81a773530930bbe
GFX23 <Spacil-Tomas@seznam.cz>
2022-09-04 15:15:53 +0200

    Update sidebar-todo.tsx

--- components/todo/sidebar-todo.tsx
+++ components/todo/sidebar-todo.tsx
@@ -1,42 +1,45 @@
-import { useState, useEffect } from "react";
+import React, { useState, useRef, MutableRefObject } from "react";
 import Dropdown from "../dropdown";
 import Input from "../input";
+import Select from "../select";
+import type { Todo } from "../../utils/types/types"
 
 interface SidebarProps {
 
 }
 
 
-const SidebarTodo: React.FC<SidebarProps> = ({
-}) => {
+const SidebarTodo: React.FC<SidebarProps> = () => {
+  const [todo, setTodo] = useState<Todo>({subject: "", description: "", state: "created", date: new Date().toISOString().substring(0, 7)})
+  const stateSelect = useRef() as MutableRefObject<HTMLSelectElement>;
 
+  const handleTodoChange = (key: string, e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    setTodo({...todo, [key]: e.target.value})
+  }
 
   return (
     <div className="w-64" aria-label="Sidebar">
       <div className="py-4 px-3 bg-gray-50 rounded dark:bg-gray-800 h-full">
-        <Dropdown name="Filtr zakázek" icon="/search.svg">
+        <Dropdown name="Vytvoř Todo" icon="/search.svg">
           <ul className="flex flex-col mt-2">
             <Input
-              name="Číslo zakázky:"
+              name="Předmět:"
               type="text"
+              value={todo.subject}
+              onChange={(e) => handleTodoChange("subject", e)}
             />
             <Input
-              name="Název dílu:"
+              name="Obsah:"
               type="text"
-              value={filter.partName}
-              onChange={(e) => handleFilterChange("partName", e)}
+              value={todo.description}
+              onChange={(e) => handleTodoChange("description", e)}
             />
-            <Input
-              name="Zákazník:"
-              type="text"
-              value={filter.customer}
-              onChange={(e) => handleFilterChange("customer", e)}
-            />
-            <Input
-              name="Datum:"
-              type="month"
-              value={filter.date}
-              onChange={(e) => handleFilterChange("date", e)}
+            <Select
+              referer={stateSelect}
+              name="Stav"
+              value={todo.state}
+              onChange={(e) => handleTodoChange("customer", e)}
+              options={["Created", "Active", "Done"]}
             />
           </ul>
         </Dropdown>
