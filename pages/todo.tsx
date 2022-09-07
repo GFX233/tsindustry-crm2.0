5a481ef3682e88ddf20827303c10e27bc0b18255
GFX23 <Spacil-Tomas@seznam.cz>
2022-09-07 21:32:45 +0200

    Update todo.tsx

--- pages/todo.tsx
+++ pages/todo.tsx
@@ -1,9 +1,11 @@
 import { NextPage } from "next"
+import { useState } from "react"
 import Head from 'next/head'
 import SidebarTodo from "../components/todo/sidebar-todo"
 import type { Todo } from "../utils/types/types"
 
 const Todo: NextPage = () => {
+    const [todos, setTodos] = useState([])
   return (
     <>
       <Head>
@@ -11,9 +13,9 @@ const Todo: NextPage = () => {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <div className="flex flex-row mt-4 justify-center max-w-5xl mx-auto">
-        <SidebarTodo />
+        <SidebarTodo setTodos={setTodos} todos={todos}/>
         <div className="shadow-md sm:rounded-lg overflow-x-auto relative w-full">
-          
+
         </div>
       </div>
     </>
