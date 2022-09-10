3a88c92e83515a498096122f4ed783b95b0571c9
GFX23 <Spacil-Tomas@seznam.cz>
2022-09-10 20:47:31 +0200

    Update todo.tsx

--- pages/todo.tsx
+++ pages/todo.tsx
@@ -3,9 +3,10 @@ import { useState } from "react"
 import Head from 'next/head'
 import SidebarTodo from "../components/todo/sidebar-todo"
 import type { Todo } from "../utils/types/types"
+import TodoCard from "../components/todo/todoCard"
 
 const Todo: NextPage = () => {
-    const [todos, setTodos] = useState([])
+    const [todos, setTodos] = useState<Todo[]>([])
   return (
     <>
       <Head>
@@ -13,9 +14,9 @@ const Todo: NextPage = () => {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <div className="flex flex-row mt-4 justify-center max-w-5xl mx-auto">
-        <SidebarTodo setTodos={setTodos} todos={todos}/>
-        <div className="shadow-md sm:rounded-lg overflow-x-auto relative w-full">
-
+        <SidebarTodo todos={todos} setTodos={setTodos}/>
+        <div className="shadow-xl sm:rounded-lg overflow-x-auto relative w-full">
+          {todos.map(todo => <TodoCard subject={todo.subject} description={todo.description} state={todo.state} date={todo.date} />)}
         </div>
       </div>
     </>
