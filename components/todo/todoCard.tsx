cc4618c8d9d394c0c72cf4a0b5add404c8866bc1
GFX23 <Spacil-Tomas@seznam.cz>
2022-09-14 20:05:43 +0200

    Update todoCard.tsx

--- components/todo/todoCard.tsx
+++ components/todo/todoCard.tsx
@@ -10,8 +10,8 @@ interface TodoProps{
 const TodoCard: React.FC<TodoProps> = ({subject, description, state, date, onClick}) => {
   return (
     <div className="block p-4 w-full bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-      <h5 className="font-normal text-gray-700 dark:text-gray-400">{subject}</h5>
-      <div className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{description}</div>
+      <div className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">{subject}</div>
+      <h5 className="font-normal text-gray-700 dark:text-gray-400">{description}</h5>
     </div>
   )
 }
