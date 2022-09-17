3898f67e19aa1bd4891c83617c6ff6b205961c95
GFX23 <Spacil-Tomas@seznam.cz>
2022-09-17 21:28:27 +0200

    Update select.tsx

--- components/select.tsx
+++ components/select.tsx
@@ -13,7 +13,7 @@ const Select: React.JSXElementConstructor<SelectProps> = ({options, name, value,
     <select ref={referer} value={value} onChange={onChange} className="block mb-4 py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200">
       <option value="" className="text-gray-900">{name}</option>
       {options.map((option, idx) => {
-        return <option key={idx} value={option.toLowerCase()}>{option}</option>
+        return <option key={idx} value={option.toUpperCase()}>{option}</option>
       })}
     </select>
   )
