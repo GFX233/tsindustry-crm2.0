744011a634e763d851d3768a754bdf9e93105cf4
GFX23 <Spacil-Tomas@seznam.cz>
2022-10-28 20:03:21 +0200

    Update avatar.tsx

--- components/avatar.tsx
+++ components/avatar.tsx
@@ -21,7 +21,7 @@ const Avatar = ({src, alt, name, email, user}: AvatarProps): JSX.Element => {
         width={45} 
         height={45}/>
       <div className="space-y-1 font-medium dark:text-white">
-          <button onClick={signOut}><div>{user?.displayName}</div></button>
+          <button onClick={signOut}><div>{user?.displayName || "Tomas"}</div></button>
           <div className="text-sm text-gray-500 dark:text-gray-400">{user?.email}</div>
       </div>
     </div>
