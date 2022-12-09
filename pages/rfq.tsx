ad2e0a185261fb3bc1a5cff74e7f202446105001
GFX23 <Spacil-Tomas@seznam.cz>
2022-12-09 22:36:08 +0100

    Update rfq.tsx

--- pages/rfq.tsx
+++ pages/rfq.tsx
@@ -1,19 +1,20 @@
-import { NextPage } from "next"
-import Head from 'next/head'
+import { NextPage } from "next";
+import Head from "next/head";
 
 const Rfq: NextPage = () => {
   return (
-<>
+    <>
       <Head>
         <title>RFQ: TS INDUSTRY SYSTEMS</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <div className="flex flex-row mt-4 justify-center max-w-5xl mx-auto">
         <div className="shadow-xl sm:rounded-lg overflow-x-auto relative w-full">
+          <p>rfq</p>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Rfq
+export default Rfq;
