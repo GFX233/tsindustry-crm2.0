cccc0609a5661afc6b30304e6d28c05315ca2424
GFX23 <Spacil-Tomas@seznam.cz>
2022-12-13 22:50:59 +0100

    Update rfq.tsx

--- pages/rfq.tsx
+++ pages/rfq.tsx
@@ -9,7 +9,7 @@ const Rfq: NextPage = () => {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <div className="flex flex-row mt-4 justify-center max-w-5xl mx-auto">
-        <div className="shadow-xl sm:rounded-lg overflow-x-auto relative w-full">
+        <div className="shadow-xl sm:rounded-lg overflow-x-auto relative w-ful bg-black">
           <p>rfq</p>
         </div>
       </div>
